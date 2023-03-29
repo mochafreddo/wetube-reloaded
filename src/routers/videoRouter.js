64e1bf53bda1b7142f45922539a2d1d929535c72
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-29 23:51:20 +0900

    6.3 Edit Video part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,9 @@
-import express from "express";
-import {
-  watch,
-  getEdit,
-  postEdit,
-  upload,
-  deleteVideo,
-} from "../controllers/videoController";
+import express from 'express';
+import {watch, getEdit, postEdit} from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.get('/:id(\\d+)', watch);
+videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import express from "express";
-import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import videoRouter from "./routers/videoRouter";
-import userRouter from "./routers/userRouter";
+import express from 'express';
+import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
 
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev");
+const logger = morgan('dev');
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
 app.use(logger);
-app.use("/", globalRouter);
-app.use("/videos", videoRouter);
-app.use("/users", userRouter);
+app.use(express.urlencoded({extended: true}));
+app.use('/', globalRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
