6ed8f318e71bf1bbec006a351aa77fad42eac7b9
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-04 01:26:40 +0900

    6.13 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,10 @@
 import Video from '../models/Video';
 
-export const home = (req, res) => {
-  console.log('Start');
-  Video.find({})
-    .exec()
-    .then((videos) => {
-      console.log('Finished');
-      res.render('home', { pageTitle: 'Home', videos });
-    })
-    .catch((error) => {});
-  console.log('I finish first');
+// Video.find({}, (error, videos) => {});
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render('home', { pageTitle: 'Home', videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
