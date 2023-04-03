5f68c667f017cf3de7f81c613cfd67ad2cb268d2
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-04 00:16:22 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
-import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import express from 'express';
+import { edit, remove, logout, see } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
-userRouter.get(":id", see);
+userRouter.get('/logout', logout);
+userRouter.get('/edit', edit);
+userRouter.get('/remove', remove);
+userRouter.get(':id', see);
 
 export default userRouter;
