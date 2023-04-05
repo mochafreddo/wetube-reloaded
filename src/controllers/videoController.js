ceea9aa44a989a0fc388437de0c92a5bc35f0d99
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-05 12:39:08 +0900

    6.20 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,29 +1,39 @@
 import Video from '../models/Video';
 
-// Video.find({}, (error, videos) => {});
-
 export const home = async (req, res) => {
   const videos = await Video.find({});
   console.log(videos);
   return res.render('home', { pageTitle: 'Home', videos });
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found.' });
+  }
   return res.render('watch', { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render('edit', { pageTitle: 'Editing' });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found.' });
+  }
+  return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   res.render('upload', { pageTitle: 'Upload Video' });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
