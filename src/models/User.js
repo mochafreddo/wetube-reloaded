25efaa043d2ced99cbb847c6c6560f3170f3dffa
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-08 14:57:23 +0900

    7.2 Creating Account part Three

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
@@ -8,5 +9,12 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
+userSchema.pre('save', async function () {
+  console.log('Users password: ', this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log('Hashed password: ', this.password);
+});
+
 const User = mongoose.model('User', userSchema);
+
 export default User;
