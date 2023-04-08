1d3fa8656fd0816dc077bff4667165573714e4aa
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-08 22:56:51 +0900

    7.8 Sessions and Cookies part Two

--- src/server.js
+++ src/server.js
@@ -22,6 +22,11 @@ app.use((req, res, next) => {
   });
 });
 
+app.get('/add-one', (req, res, next) => {
+  req.session.potato += 1;
+  res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
