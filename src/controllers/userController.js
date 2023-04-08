421c0c91755306df0b0a160bfa7dd1a8906d48be
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-08 16:13:52 +0900

    7.6 Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import User from '../models/User';
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
@@ -39,18 +40,23 @@ export const getLogin = (req, res) =>
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
-    return res
-      .status(400)
-      .render('login', {
-        pageTitle: 'Login',
-        errorMessage: 'An account with username does not exists.',
-      });
+  const pageTitle = 'Login';
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'An account with username does not exists.',
+    });
   }
-  // check if account exists
-  // check if password is correct
-  res.end();
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'Wrong password.',
+    });
+  }
+  console.log('LOG USER IN! COMING SOON!');
+  return res.redirect('/');
 };
 
 export const edit = (req, res) => res.send('Edit User');
