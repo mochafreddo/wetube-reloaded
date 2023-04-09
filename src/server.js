adf2d1f5e6762fa9e70d11fba4837aedf20d298c
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-09 23:19:59 +0900

    7.12 MongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
@@ -14,7 +15,14 @@ app.set('views', `${process.cwd()}/src/views`);
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({ secret: 'Hello!', resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: 'Hello!',
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube' }),
+  }),
+);
 
 app.use(localsMiddleware);
 app.use('/', rootRouter);
