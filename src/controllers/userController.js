0bc8f761e6f47aa5248b0843296e21b7fbd25a95
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-15 02:16:03 +0900

    7.21 Github Login part Six

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -99,7 +99,6 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    console.log(userData);
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
@@ -107,15 +106,32 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.fine(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true,
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect('/login');
     }
-  } else {
-    return res.redirect('/login');
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect('/');
+    }
+    const user = await User.create({
+      name: userData.name,
+      username: userData.login,
+      email: emailObj.email,
+      password: '',
+      location: userData.location,
+      socialOnly: true,
+      avatarUrl: userData.avatar_url,
+    });
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect('/');
   }
+  return res.redirect('/login');
 };
 
 export const edit = (req, res) => res.send('Edit User');
--- src/models/User.js
+++ src/models/User.js
@@ -3,16 +3,15 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
 
 userSchema.pre('save', async function () {
-  console.log('Users password: ', this.password);
   this.password = await bcrypt.hash(this.password, 5);
-  console.log('Hashed password: ', this.password);
 });
 
 const User = mongoose.model('User', userSchema);
