7952c7e3b6f96918beb4a337ecc111d054c6c0e1
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-22 17:15:25 +0900

    8.0 Edit Profile GET

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,5 +137,7 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect('/');
 };
-export const edit = (req, res) => res.send('Edit User');
+export const getEdit = (req, res) =>
+  res.render('edit-profile', { pageTitle: 'Edit Profile' });
+export const postEdit = (req, res) => res.render('edit-profile');
 export const see = (req, res) => res.send('See User');
