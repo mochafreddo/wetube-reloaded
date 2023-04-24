71795a6b4df5d80a2c0ec83192d2c42ef222979e
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-25 00:57:03 +0900

    #8.10 User Profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -18,18 +18,22 @@ import {
 const userRouter = express.Router();
 
 userRouter.get('/logout', protectorMiddleware, logout);
+
 userRouter
   .route('/edit')
   .all(protectorMiddleware)
   .get(getEdit)
   .post(avatarUpload.single('avatar'), postEdit);
+
 userRouter
   .route('/change-password')
   .all(protectorMiddleware)
   .get(getChangePassword)
   .post(postChangePassword);
+
 userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
 userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(':id', see);
+
+userRouter.get('/:id', see);
 
 export default userRouter;
