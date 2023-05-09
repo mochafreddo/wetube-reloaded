15109ca1db14234c508fbac151b37757a249ffc7
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-05-10 02:32:55 +0900

    #13.4 Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,16 @@ const video = document.getElementById('preview');
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement('a');
+  a.href = videoFile;
+  // download 속성은 a 요소가 리소스를 다운로드할 때 사용할 파일 이름을 나타냅니다.
+  a.download = 'MyRecording.webm';
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = 'Download Recording';
@@ -17,10 +25,10 @@ const handleStart = () => {
   startBtn.innerText = 'Stop Recording';
   startBtn.removeEventListener('click', handleStart);
   startBtn.addEventListener('click', handleStop);
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
   recorder.ondataavailable = (event) => {
     // createObjectURL() 메서드는 지정된 미디어 객체나 미디어 스트림을 가리키는 URL을 생성합니다.
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
