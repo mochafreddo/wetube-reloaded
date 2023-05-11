089420c629e6dba3a4301c701799d91e51cc2c04
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-05-12 02:52:17 +0900

    #16.5 Commenting

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from '../models/Video';
+import Comment from '../models/Comment';
 import User from '../models/User';
 
 export const home = async (req, res) => {
@@ -127,7 +128,19 @@ export const registerView = async (req, res) => {
 };
 
 export const createComment = async (req, res) => {
-  console.log(req.params);
-  console.log(req.body.text);
-  return res.end();
+  const { session: { user }, body: { text }, params: { id } } = req;
+
+  const video = await Video.findById(id);
+
+  if (!video) {
+    return res.sendStatus(404);
+  }
+
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+
+  return res.sendStatus(201);
 };
