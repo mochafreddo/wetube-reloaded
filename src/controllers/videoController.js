66a0d6c63eb923da8bd1a9f9d42cdc00e2fd5047
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-05-12 03:08:21 +0900

    #16.6 Rendering Comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate('owner');
+  const video = await Video.findById(id).populate('owner').populate('comments');
+  console.log(video);
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' });
   }
@@ -129,18 +130,16 @@ export const registerView = async (req, res) => {
 
 export const createComment = async (req, res) => {
   const { session: { user }, body: { text }, params: { id } } = req;
-
   const video = await Video.findById(id);
-
   if (!video) {
     return res.sendStatus(404);
   }
-
   const comment = await Comment.create({
     text,
     owner: user._id,
     video: id,
   });
-
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
