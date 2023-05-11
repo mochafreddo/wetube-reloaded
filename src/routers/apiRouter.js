45175766784f46f1315b2551263ab1795473aa76
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-05-11 22:36:09 +0900

    #16.3 API Route part One

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,9 @@
 import espress from 'express';
-import { registerView } from '../controllers/videoController';
+import { registerView, createComment } from '../controllers/videoController';
 
 const apiRouter = espress.Router();
 
 apiRouter.post('/videos/:id([0-9a-f]{24})/view', registerView);
+apiRouter.post('/videos/:id([0-9a-f]{24})/comment', createComment);
 
 export default apiRouter;
