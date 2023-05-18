cf1dec6dadfd6d21f2d0d7b9ff549c3a4dfc179c
mochafreddo <corc411@gmail.com>
2023-05-19 01:16:49 +0900

    refactor(s3): use environment variables for AWS region and bucket
    - AWS_REGION and AWS_BUCKET_NAME are now pulled from environment variables, allowing for greater flexibility and security in configuration.
    - Updated the createS3Uploader function in src/s3.js to use these environment variables.

--- src/s3.js
+++ src/s3.js
@@ -2,13 +2,13 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 
-const REGION = 'us-east-1';
+const REGION = process.env.AWS_REGION;
 const s3Client = new S3Client({ region: REGION });
 
 export function createS3Uploader(path) {
   return multerS3({
     s3: s3Client,
-    bucket: 'wetube-y8rv',
+    bucket: process.env.AWS_BUCKET_NAME,
     acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
